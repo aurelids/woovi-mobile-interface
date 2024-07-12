f2a923615cffe9a11ec080028faeb19d2904ea91
Gabriel Cogo <gabrielaurelio33@gmail.com>
2024-07-12 13:24:56 -0300

    first commit

--- src/pages/CardPaymentPage.js
+++ src/pages/CardPaymentPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
-import CardPaymentForm from '../components/CardPaymentForm';
+import CardPaymentFormin from '../components/CardPaymentForm';
 import Footer from '../components/Footer';
 
 const CardPaymentPage = () => {
